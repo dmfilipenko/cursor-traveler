b4e76d9cd987a80b989809996d8745b29265983e
dmfilipenko <wind.fd@gmail.com>
2018-09-23 22:23:57 +0300

    updates icons

--- sources/src/popup/index.ts
+++ sources/src/popup/index.ts
@@ -1,27 +1,20 @@
 import { pipe, sum, values } from 'ramda';
-import { map, switchMap, tap } from 'rxjs/operators';
 
-import { getDateTimestamp } from '../utils/date';
-import { message$ } from '../utils/eventMessage';
-import { getStorage$, storageChange$ } from '../utils/getFromStorage';
+// import { map, switchMap, tap } from 'rxjs/operators';
+// import { getDateTimestamp } from '../utils/date';
+// import { message$ } from '../utils/eventMessage';
+import { storageChange$ } from '../utils/getFromStorage';
 
-const backgroundPage = chrome.extension.getBackgroundPage();
+// const backgroundPage = chrome.extension.getBackgroundPage();
 const $totalDistance = document.querySelector('#total-distance')
 
-getStorage$(`${getDateTimestamp()}`).subscribe(dist => {
-    $totalDistance.innerHTML = `${dist}`;
-})
 
-
-const setValue = v => $totalDistance.innerHTML = v
-storageChange$.subscribe(
-    () => {
-        chrome.storage.local.get(
-            pipe(
-                values,
-                sum,
-                setValue
-            )
-        )
-    }
+const setValue = (v: number = 0) => $totalDistance.innerHTML = `${v.toFixed(2)} cm`
+const totalValue = pipe(
+    values,
+    sum,
+    setValue
 )
+
+chrome.storage.local.get(totalValue)
+storageChange$.subscribe(() => chrome.storage.local.get(totalValue))
