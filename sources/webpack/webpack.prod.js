733fa60560add0c4de8fb7bc98a61103361251d8
Dmytro Filipenko <dmytrof@wix.com>
2018-09-24 15:14:40 +0300

    update icons

--- sources/webpack/webpack.prod.js
+++ sources/webpack/webpack.prod.js
@@ -2,6 +2,8 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+const ZipFilesPlugin = require('webpack-zip-files-plugin');
+const path = require('path')
 
 module.exports = merge(common, {
     mode: 'production',
