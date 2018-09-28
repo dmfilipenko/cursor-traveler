011ec870a3bcfdb30e4182b6a03ffb1c6ff09bfd
Dmytro Filipenko <dmytrof@wix.com>
2018-09-28 13:50:50 +0300

    add jest tests

--- sources/src/utils/total.ts
+++ sources/src/utils/total.ts
@@ -1,6 +1,6 @@
-import pipe from 'ramda/es/pipe';
-import sum from 'ramda/es/sum';
-import values from 'ramda/es/values';
+import * as pipe from 'ramda/src/pipe';
+import * as sum from 'ramda/src/sum';
+import * as values from 'ramda/src/values';
 
 export const calculateTotal = pipe(
     values,
