14857ba872c495bb0a6c8d964589242369b6b92d
Dmytro Filipenko <dmytrof@wix.com>
2018-09-28 14:25:22 +0300

    fix issue

--- sources/src/utils/badge.ts
+++ sources/src/utils/badge.ts
@@ -2,12 +2,12 @@ import * as pipe from 'ramda/src/pipe';
 
 import { getUnits } from './converter';
 import { calculateTotal, totalToBadge } from './total';
+import {tap} from 'rxjs/internal/operators/tap';
 
 export const setBadge = text => chrome.browserAction.setBadgeText({ text })
 export const setTotalToBadge = () => chrome.storage.local.get(
     pipe(
         calculateTotal,
-        totalToBadge,
         getUnits,
         setBadge,
     )
