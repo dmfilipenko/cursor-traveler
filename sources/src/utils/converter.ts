114081986575229260d6b2a60a4d6070f6905975
dmfilipenko <wind.fd@gmail.com>
2018-09-30 23:57:20 +0300

    optimization

--- sources/src/utils/converter.ts
+++ sources/src/utils/converter.ts
@@ -1,16 +1,20 @@
 import * as always from 'ramda/src/always';
 import * as cond from 'ramda/src/cond';
+import * as curry from 'ramda/src/curry';
 import * as lte from 'ramda/src/lte';
+import * as multiply from 'ramda/src/multiply';
 import * as pipe from 'ramda/src/pipe';
-import * as  T from 'ramda/src/T';
+import * as T from 'ramda/src/T';
 
 import { Units } from '../types/enums';
 
-const cmToM = (cm: number) => 0.01 * cm
-const mToKm = (m: number) => 0.001 * m
+export const pxToCm = multiply(0.02645833)
 
-const addMetric = (metric) => (v) => `${v} ${metric}`
-const fixed = n => v => v.toFixed(n)
+const cmToM = multiply(0.01)
+const mToKm = multiply(0.001)
+
+const addMetric = curry((metric, v) => `${v} ${metric}`)
+const fixed = curry((n, v) => v.toFixed(n))
 export const convertMetrics = cond([
   [lte(10 ** 5), pipe(cmToM, mToKm, fixed(3), addMetric(Units.KILOMETR))],
   [lte(10 ** 2), pipe(cmToM, fixed(2), addMetric(Units.METER))],
