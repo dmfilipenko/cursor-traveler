92de71e4f351d92ea6c4b2efccf251d838911b62
Daria <dariashvydka@gmail.com>
2018-10-04 21:48:56 +0300

    Added styles, font and close button

--- sources/src/content/popup/view.tsx
+++ sources/src/content/popup/view.tsx
@@ -1,4 +1,4 @@
-import { h, render } from 'preact';
+import { h, Component, render } from 'preact';
 import { compose, lifecycle } from 'recompose';
 import * as style from './test.css';
 
@@ -7,23 +7,38 @@ const div = document.createElement('div')
 div.classList.add('cursor-traveler-container')
 document.body.appendChild(div);
 
-const TitleComponent = () => (
-    <span className={style.title}>You traveled 🏁</span>
-)
+class DemoComponent extends Component <{}, { isToggleOn: boolean }>{
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+        this.handleClick = this.handleClick.bind(this);
+    }
 
+    handleClick() {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+    }
 
-const DataComponent = (props) => (
-    <div className={style.data}>
-        <div className={style.number}>
-            155<span>.123</span>
-        </div>
-        <div className={style.unit}>kilometres</div>
-    </div>
-)
+    render() {
+        return (
+            <div>
+                {this.state.isToggleOn && (
+                    <div className={style.wrapper}>
+                        <button className={style.close} onClick={this.handleClick}>
+                        </button>
+                        <span className={style.title}>You traveled 🏁</span>
+                        <div className={style.data}>
+                            <div className={style.number}>
+                                155<span>.123</span>
+                            </div>
+                            <div className={style.unit}>kilometres</div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
 
-render(
-    <div className={style.wrapper}>
-             <TitleComponent />
-             <DataComponent />
-         </div>
-, div);
+render(<DemoComponent />, div);
