4af52901c16dcb04b3998299b0d8f26db803393f
Dmytro Filipenko <dmytrof@wix.com>
2018-10-11 13:44:48 +0300

    refactor converter

--- sources/src/utils/converter.ts
+++ sources/src/utils/converter.ts
@@ -14,49 +14,31 @@ export const pxToCm = multiply(0.02645833)
 
 const cmToM = multiply(0.01)
 const mToKm = multiply(0.001)
+const cmToKm = pipe(cmToM, mToKm)
 
 const addMetric = curry((metric, v) => [v, metric])
 const fixed = curry((n, v) => v.toFixed(n))
-const addPluralToMetric = ([v, metric]) => {
-  debugger
-  return [v, addPluralForm(v, metric)]
-}
+const addPluralToMetric = ([v, metric]) => [v, addPluralForm(v, metric)]
+const unitRounding = (round = always, tranform = always) => pipe(
+  tranform,
+  round,
+  parseFloat
+)
+const metricAndValue = (metric) => pipe(
+  addMetric(metric),
+  addPluralToMetric
+)
+const roundKm = unitRounding(fixed(3), cmToKm)
+const roundM = unitRounding(fixed(2), cmToM)
+const roundCm = unitRounding(fixed(2), cmToM)
+const roundAndMetric = (round, metric) => pipe(
+  round, 
+  metricAndValue(metric)
+)
 export const convertMetrics = cond([
-  [
-    lte(10 ** 5), 
-    pipe(
-      cmToM, 
-      mToKm, 
-      fixed(3),
-      pipe(
-        addMetric(UnitsFull.KILOMETR),
-        addPluralToMetric
-      )
-    )
-  ],
-  [
-    lte(10 ** 2), 
-    pipe(
-      cmToM, 
-      fixed(2), 
-      parseFloat,
-      pipe(
-        addMetric(UnitsFull.METER),
-        addPluralToMetric
-      )
-    )
-    ],
-  [
-    T, 
-    pipe(
-      fixed(2), 
-      parseFloat,
-      pipe(
-        addMetric(UnitsFull.CENTIMETER),
-        addPluralToMetric
-      )
-    )
-  ]
+  [lte(10 ** 5), roundAndMetric(roundKm, UnitsFull.KILOMETR)],
+  [lte(10 ** 2), roundAndMetric(roundM, UnitsFull.METER)],
+  [T, roundAndMetric(roundCm, UnitsFull.CENTIMETER)]
 ])
 export const getUnits = cond([
   [lte(10 ** 5), always(UnitsShort.KILOMETR)],
