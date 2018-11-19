bf826b340b6b60039a11b1cb5200de0433d4122b
dmfilipenko <wind.fd@gmail.com>
2018-11-19 21:26:13 +0200

    remove shitty react

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,38 +1,29 @@
 import * as pipe from 'ramda/src/pipe';
-import * as React from 'react';
-import { render } from 'react-dom';
 
 import { convertMetrics } from '../utils/converter';
 import { storageChange$ } from '../utils/getFromStorage';
 import { calculateTotal } from '../utils/total';
-import * as s from './styles.css';
 
-const div = document.querySelector('#root')
-const View = (props: {total: number, metric: string}) => {
-    const [decimal, mantissa] = props.total.toString().split('.')
-    return (
-        <div className={s.wrapper}>
-            <div className={s.title}>Mouse traveled</div>
-            <div className={s.total}>
-                {decimal}<span>.{mantissa}</span>
-            </div>
-            {props.metric && <div className={s.unit}>{props.metric}</div>}
-        </div>
-    )
+const render = (total: number, metric: string) => {
+    const [decimal, mantissa] = total.toString().split('.')
+    const $total = document.querySelector('.total')
+    const $unit = document.querySelector('.unit')
+    const $mantissa = document.createElement('span')
+
+    $total.textContent = decimal
+    if (mantissa) $mantissa.textContent = `.${mantissa}`
+    $total.appendChild($mantissa)
+
+    if (metric) $unit.textContent = metric
 }
 
 const renderPopup = () => chrome.storage.local.get(
     pipe(
         calculateTotal,
         convertMetrics,
-        ([ total = 0, metric ]) => render(<View metric={metric} total={total} />, div)
+        ([ total = 0, metric ]) => render(total, metric)
     )
 )
 
 renderPopup()
 storageChange$.subscribe(renderPopup)
-
-// window.onblur = function(){
-//     chrome.browserAction.setBadgeText({"text":"ABCD"});
-// }
-
