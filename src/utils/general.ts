f5a386df081768c50df1bbec2139232a444a84cb
Dmytro Filipenko <dmytrof@wix.com>
2018-12-27 19:44:12 +0200

    add tests

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,3 +1,4 @@
-import { identity, unapply } from 'ramda';
+import { identity, unapply, pipe, head, concat } from 'ramda';
 
-export const argsToList = unapply(identity);
+// export const argsToList = unapply(identity);
+export const argsToList = (...args) => args;
--- src/utils/plural.ts
+++ src/utils/plural.ts
@@ -1,6 +1,4 @@
-import { cond, lte, always, curryN, T, pipe, last, head } from 'ramda'
-import * as equals from 'ramda/src/equals';
-import * as and from 'ramda/src/and';
+import { cond, equals, always, lte, converge, and, curryN, T, pipe, last, identity, head, concat, __ } from 'ramda'
 import { UnitsFull } from '../types/enums';
 import {argsToList} from './general';
 const lessOrEqualTwo = pipe(
@@ -8,16 +6,18 @@ const lessOrEqualTwo = pipe(
   n => last<number>(n),
   lte(2)
 )
-
+const addS = pipe(argsToList, head, v => concat(v, 's'))
+const feetEdgeCase = converge(
+  and,
+  [
+    lessOrEqualTwo,
+    equals(UnitsFull.FOOT)
+  ]
+)
 export const addPluralForm = curryN(2, cond([
-  [lessOrEqualTwo, pipe(
-    head,
-    v => always(`${v}s`)
-  )],
-  [lte(2), pipe(
-    head,
-    v => always(`${v}s`)
-  )],
-  [T, v => always(v)]
+  [feetEdgeCase, always(UnitsFull.FEET)],
+  [lessOrEqualTwo, addS],
+  [T, identity]
 ]))
+
 // export const addPluralForm = curryN(2, (metric: string, val: number) => pluralForm(metric)(val))
